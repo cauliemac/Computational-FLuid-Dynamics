b0127747e65647869b63338c7d71319c0a9b9de3
cauliemac <39170797+cauliemac@users.noreply.github.com>
2021-03-13 01:22:00 +0000

    added gladiotor
    gladiator fights are now standard in all_cents

--- all_cents.c
+++ all_cents.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
 int main ()
 {
     float cents = 0.00; //current cent count
@@ -14,4 +15,20 @@ int main ()
         printf("Was it %.2f euro?\n", cents);
         usleep(400000);
     }
+}
+*/
+
+int main()
+{
+    sleep(3);
+    for (int i = 0; i < 5; i++)
+    {
+        int count = 5-i;
+        printf("starting gladiator games in %i\n",count);
+        sleep(1);
+    }
+    //sleep(1);
+    printf("\nAND SUPRISE SUPRISE\n");
+    sleep(2);
+    printf("\nTHOMAS WON");
 }
