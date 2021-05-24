d80a6256ce1f8345c99b57ceddf8c5f14b012e69
cauliemac <39170797+cauliemac@users.noreply.github.com>
2021-05-24 07:44:20 +0100

    pressure not working :(
    when I do the sum by hand it works. but not in code

--- EulerEquation.c
+++ EulerEquation.c
@@ -266,19 +266,16 @@ void GodunovScheme (cell_state temp_cell_state, cell_state* solution_cell_state,
         c = sound_speed * temp_cell_state.Velocity[j];//signal speed
 
         //Use Godunov method to update the value of the cell.
-        solution_conserve.Mass[j] = temp_conserve.Mass[j] + (c*dt/dx) * (MassRight - MassLeft);// - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Mass,j,slope_limiter_type));
-        solution_conserve.Momentum[j] = temp_conserve.Momentum[j] + ((c*dt/dx) * (MomentumRight - MomentumLeft));// - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Momentum,j,slope_limiter_type));
-        solution_conserve.Energy[j] = temp_conserve.Energy[j] + (c*dt/dx) * (EnergyRight - EnergyLeft);// - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Energy,j,slope_limiter_type));
-
-        printf("temp cons mom is %f\n",temp_conserve.Momentum[j]);
-        printf("solution cons mom is %f\n",solution_conserve.Momentum[j]);
-        system("pause");
+        solution_conserve.Mass[j] = temp_conserve.Mass[j] + (c*dt/dx) * (MassRight - MassLeft) - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Mass,j,slope_limiter_type));
+        solution_conserve.Momentum[j] = temp_conserve.Momentum[j] + ((c*dt/dx) * (MomentumRight - MomentumLeft)) - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Momentum,j,slope_limiter_type));
+        solution_conserve.Energy[j] = temp_conserve.Energy[j] + (c*dt/dx) * (EnergyRight - EnergyLeft) - 0.5 * (c*dt/dx) * (dx - c*dt)*(chooseSlopeLimiter(temp_conserve.Energy,j,slope_limiter_type));
 
         //convert back to primitave variables and return solution_cell_state
         solution_cell_state->Density[j] = solution_conserve.Mass[j] / dx;
         solution_cell_state->Velocity[j] = sqrt(solution_conserve.Momentum[j]/solution_cell_state->Density[j]) / dx;
-        solution_cell_state->Pressure[j] = (solution_conserve.Energy[j]/dx - 0.5 * solution_cell_state->Density[j] * pow(solution_cell_state->Velocity[j],2)) * (2/3);
+        solution_cell_state->Pressure[j] = ((solution_conserve.Energy[j]/dx) - (0.5 * solution_cell_state->Density[j] * pow(solution_cell_state->Velocity[j],2))) * (2/3);
         
+        /*
         printf("-------------------------------------------------------\n");
         printf("for cell j=%d\n",j);
         printf("-------------------------------------------------------\n");
@@ -308,7 +305,7 @@ void GodunovScheme (cell_state temp_cell_state, cell_state* solution_cell_state,
         printf("Pressure Solution is %f\n\n",solution_cell_state->Pressure[j]);
         
         system("pause");
-        
+        */
 
         //Conservation of Momentum = rho*u
         //Conservation of Energy = (rho * u^2) + p
